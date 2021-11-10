4aef92b8cd80f79df63eab7b361d161e52686fec
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-10 18:06:28 +0100

    Creata struttura
    Creata struttura usando Vue CLI, aggiungendo il componente Title.vue.

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Title">
+      <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Title{
+    margin-top: 50px;
+    text-align: center;
+}
+
+</style>
